43c88b23132a017908c565c98e622e9771e82d60
Elrich Groenewald <elrgro@gmail.com>
2020-12-10 14:43:30 +0200

    Num epochs config option

--- irccam/training/train.py
+++ irccam/training/train.py
@@ -15,27 +15,20 @@ def train(config):
 
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     logger = TestTubeLogger(
-        save_dir=os.path.join(
-            config.log_dir,
-            "{}-{}".format(timestamp, config.experiment_name),
-        ),
+        save_dir=os.path.join(config.log_dir, "{}-{}".format(timestamp, config.experiment_name)),
         name="tube_logs",
         version=0,
     )
 
     checkpoint_callback = ModelCheckpoint(
-        save_last=True,
-        save_top_k=1,
-        verbose=True,
-        monitor="val_iou",
-        mode="max",
-        prefix="",
+        save_last=True, save_top_k=1, verbose=True, monitor="val_iou", mode="max", prefix=""
     )
 
     trainer = Trainer(
         logger=logger,
         checkpoint_callback=checkpoint_callback,
         gpus="-1" if torch.cuda.is_available() else None,
+        max_nb_epochs=config.num_epochs,
     )
 
     trainer.fit(model)
@@ -48,21 +41,14 @@ def get_config(config_file):
         config = json.load(f)
     if not os.path.isdir(config["dataset_root"]):
         config["dataset_root"] = os.path.join(DATASET_PATH, config["dataset_root"])
-    config["log_dir"] = config.get(
-        "log_dir", os.path.join(PROJECT_PATH, "training_logs")
-    )
+    config["log_dir"] = config.get("log_dir", os.path.join(PROJECT_PATH, "training_logs"))
     return argparse.Namespace(**config)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-c",
-        "--config",
-        default=None,
-        required=True,
-        type=str,
-        help="config file path (default: None)",
+        "-c", "--config", default=None, required=True, type=str, help="config file path (default: None)",
     )
     args = parser.parse_args()
     config = get_config(args.config)
