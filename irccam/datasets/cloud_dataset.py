Sun masking fixes, dataset description

irccam/datasets/cloud_dataset.py
@@ -1,8 +1,16 @@
+import cv2
 from torch.utils.data import Dataset
 import os
 import h5py
 import numpy as np
 from bisect import bisect_right
+
+def create_label_image(labels):
+    img = np.zeros((labels.shape[0], labels.shape[1], 3))
+    img[:, :, 0] = labels * 255
+    img[np.where(labels == -1)] = float("nan")
+    return img
+
 
 """
 Implemented the dataset from daily H5 files
@@ -93,10 +101,10 @@ class HDF5Dataset(Dataset):
 
 
 if __name__ == "__main__":
-    dataset = HDF5Dataset("../../data/datasets/main_2", "train")
+    dataset = HDF5Dataset("../../data/datasets/main_3", "test", use_sun_mask=True)
     print(len(dataset))
-    print(dataset[23]["label"])
+    print(dataset[14]["timestamp"])
 
-    dataset = HDF5Dataset("../../data/datasets/optimized_2", "train")
+    dataset = HDF5Dataset("../../data/datasets/optimized_3", "test")
     print(len(dataset))
-    print(dataset[23]["label"])
+    print(dataset[23]["timestamp"])

irccam/datasets/image_processing.py
@@ -49,19 +49,19 @@ Find sun on IR and replaces it with clear sky
 """
 
 
-def sun_correction(vis_img, ir_img, cs_img, labels, treshold=235):
+def sun_correction(vis_img, ir_img, cs_img, labels, threshold=235):
     # find the highsest 50 pixels
     img = ir_img.copy()
     img = cv2.GaussianBlur(img, (3, 3), 0)
     (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(img)
-    if maxVal > treshold:
+    if maxVal > threshold:
         # lets call this a sun
         cv2.circle(img, maxLoc, 40, -1337, -1)
-
         # draw circle on vis
         cv2.circle(vis_img, maxLoc, 40, (0, 0, 255), 3)
         return img == -1337
-    return np.ones(img.shape, dtype=bool)
+
+    return np.zeros(img.shape, dtype=bool)
 
 
 def normalize_irccam_image(img_ir):